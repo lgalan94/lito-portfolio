ffdb04c09476c2fb3c3758e4e1174b7e22c8cbaf
Lito Galan Jr <galanlito.94@gmail.com>
2025-09-23 15:40:35 +0800

    remove the unneccesary files

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1 className='text-3xl font-bold text-red-700 underline'>This is h1</h1>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-5xl font-bold text-red-700 underline">This is a Developer Portfolio</h1>
       </div>
     </>
   )
