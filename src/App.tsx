f513de9f6fae5a8011c8c279fc45f04de12ada4c
Lito Galan Jr <galanlito.94@gmail.com>
2025-09-23 15:28:51 +0800

    Add portfolio skeletal files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold text-red-700 underline'>This is h1</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
