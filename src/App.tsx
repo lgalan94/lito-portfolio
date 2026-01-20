1ce98af164650f416ab6eea0486fe5c1087b105d
Lito Galan Jr <132322356+lgalan94@users.noreply.github.com>
2026-01-21 00:49:45 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Hero from "./components/Hero";
+
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
