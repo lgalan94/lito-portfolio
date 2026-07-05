c4819cba10e13b7cc38fa5c3a52724a09e2d2af5
Lito Galan Jr <132322356+lgalan94@users.noreply.github.com>
2026-07-05 16:17:17 +0800

    Refactor Projects component for better readability

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -173,7 +173,7 @@ const Projects: React.FC = () => {
         <motion.div
           layout
           key={activeCategory}
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 !mx-auto !px-20 gap-8"
           variants={containerVariants}
           initial="hidden"
           animate="visible"
